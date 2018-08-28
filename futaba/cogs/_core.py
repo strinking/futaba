ff137390db1663ef1ee500f4859b7f8b5a1292da
Jake Richardson <jakey8180@gmail.com>
2018-08-28 04:06:03 +0100

    Added cog core template

---
+++ futaba/cogs/_core.py
@@ -0,0 +1,30 @@
+#
+# cogs/<Cog Folder Name>/core.py
+#
+# futaba - A Discord Mod bot for the Programming server
+# Copyright (c) 2017 Jake Richardson, Ammon Smith, jackylam5
+#
+# futaba is available free of charge under the terms of the MIT
+# License. You are free to redistribute and/or modify it under those
+# terms. It is distributed in the hopes that it will be useful, but
+# WITHOUT ANY WARRANTY. See the LICENSE file for more details.
+#
+
+'''
+<description>
+'''
+import asyncio
+import logging
+
+import discord
+from discord.ext import commands
+
+logger = logging.getLogger(__package__)
+
+class <Classname>:
+    '''
+    <description>
+    '''
+
+    def __init__(self, bot):
+        self.bot = bot
