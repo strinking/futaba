ab045f77e8ba5ce0a8d06a3e4247eb1edf548652
Ammon Smith <ammon.i.smith@gmail.com>
2018-09-15 03:22:55 -0400

    Add about bot command.

--- futaba/cogs/misc/core.py
+++ futaba/cogs/misc/core.py
@@ -16,13 +16,15 @@ Cog for misceallaneous commands that don't really belong anywhere else.
 
 import asyncio
 import logging
+import sys
 from datetime import datetime
 
 import discord
 from discord.ext import commands
 
-from futaba import permissions
+from futaba import permissions, __version__
 from futaba.enums import Reactions
+from futaba.utils import GIT_HASH, fancy_timedelta
 
 logger = logging.getLogger(__name__)
 
@@ -42,16 +44,41 @@ class Miscellaneous:
 
     @commands.command(name='ping')
     async def ping(self, ctx):
-        '''
-        Determines the bot's current latency.
-        '''
+        ''' Determines the bot's current latency. '''
 
         duration = datetime.now() - discord.utils.snowflake_time(ctx.message.id)
         ms = duration.microseconds / 1000
 
         await asyncio.gather(
             Reactions.SUCCESS.add(ctx.message),
-            ctx.send(content=f"Pong! (`{ms} ms`)")
+            ctx.send(content=f"Pong! `{ms} ms`")
+        )
+
+    @commands.command(name='about', aliases=['aboutme', 'botinfo'])
+    async def about(self, ctx):
+        ''' Prints information about the running bot. '''
+
+        pyver = sys.version_info
+        python_emoji = self.bot.get_emoji(490419105699069952) or ''
+        discord_emoji = self.bot.get_emoji(490419059964510210) or ''
+
+        embed = discord.Embed()
+        embed.set_thumbnail(url=self.bot.user.avatar_url)
+        embed.set_author(name=f'Futaba v{__version__} {GIT_HASH}')
+        embed.add_field(name='Running for', value=fancy_timedelta(self.bot.uptime))
+        embed.add_field(name='Created by', value='https://discord.gg/010z0Kw1A9ql5c1Qe')
+        embed.add_field(name='Source code', value='https://github.com/strinking/futaba')
+        embed.description = '\n'.join((
+            f'{python_emoji} Powered by Python {pyver.major}.{pyver.minor}.{pyver.micro}',
+            f'{discord_emoji} Using discord.py {discord.__version__}',
+        ))
+
+        if ctx.guild is not None:
+            embed.colour = ctx.guild.me.colour
+
+        await asyncio.gather(
+            Reactions.SUCCESS.add(ctx.message),
+            ctx.send(embed=embed),
         )
 
     @commands.command(name='shutdown', aliases=['halt'])
