50bd312cd0c7e436111a9c39b9821727352a087b
Ammon Smith <ammon.i.smith@gmail.com>
2018-09-20 15:39:00 -0400

    Add unicode utility module.

---
+++ futaba/unicode.py
@@ -0,0 +1,146 @@
+#
+# unicode.py
+#
+# futaba - A Discord Mod bot for the Programming server
+# Copyright (c) 2017-2018 Jake Richardson, Ammon Smith, jackylam5
+#
+# futaba is available free of charge under the terms of the MIT
+# License. You are free to redistribute and/or modify it under those
+# terms. It is distributed in the hopes that it will be useful, but
+# WITHOUT ANY WARRANTY. See the LICENSE file for more details.
+#
+
+import logging
+import os
+import re
+import string
+import unicodedata
+from bisect import bisect
+from urllib.request import urlretrieve
+
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    'READABLE_CHAR_SET',
+    'UNICODE_BLOCKS',
+    'UNICODE_BLOCKS_FILENAME',
+    'UNICODE_CATEGORY_NAME',
+    'normalize_caseless',
+    'unicode_block',
+    'unicode_repr',
+]
+
+READABLE_CHAR_SET = frozenset(string.printable) - frozenset('\t\n\r\x0b\x0c')
+
+# Adapted from https://gist.github.com/acdha/49a610089c2798db6fe2
+def _load_unicode_blocks():
+    if not os.path.exists(UNICODE_BLOCKS_FILENAME):
+        logger.info("Unicode blocks file '%s' does not exist, downloading...", UNICODE_BLOCKS_FILENAME)
+        urlretrieve('https://unicode.org/Public/UNIDATA/Blocks.txt', filename=UNICODE_BLOCKS_FILENAME)
+
+    blocks = []
+    with open(UNICODE_BLOCKS_FILENAME) as fh:
+        content = fh.read()
+
+    for start, end, block_name in re.findall(r'([0-9A-F]+)\.\.([0-9A-F]+);\ (\S.*\S)', content):
+        if block_name == 'No_Block':
+            continue
+
+        blocks.append((int(start, 16), int(end, 16), block_name))
+    return blocks
+
+UNICODE_BLOCKS_FILENAME = 'unidata-blocks.txt'
+UNICODE_BLOCKS = _load_unicode_blocks()
+UNICODE_BLOCK_STARTS = [block[0] for block in UNICODE_BLOCKS]
+
+UNICODE_CATEGORY_NAME = {
+    'Lu': 'Letter, uppercase',
+    'Ll': 'Letter, lowercase',
+    'Lt': 'Letter, titlecase',
+    'Lm': 'Letter, modified',
+    'Lo': 'Letter, other',
+    'Mn': 'Mark, nonspacing',
+    'Mc': 'Mark, spacing combining',
+    'Me': 'Mark, enclosing',
+    'Nd': 'Number, decimal digit',
+    'Nl': 'Number, letter',
+    'No': 'Number, other',
+    'Pc': 'Punctuation, connector',
+    'Pd': 'Punctuation, dash',
+    'Ps': 'Punctuation, open',
+    'Pe': 'Punctuation, close',
+    'Pi': 'Punctuation, initial quote',
+    'Pf': 'Punctuation, final quote',
+    'Po': 'Punctuation, other',
+    'Sm': 'Symbol, mathematics',
+    'Sc': 'Symbol, currency',
+    'Sk': 'Symbol, modifier',
+    'So': 'Symbol, other',
+    'Zs': 'Separator, space',
+    'Zl': 'Separator, line',
+    'Zp': 'Separator, paragraph',
+    'Cc': 'Other, control',
+    'Cf': 'Other, format',
+    'Cs': 'Other, surrogate',
+    'Co': 'Other, private use',
+    'Cn': 'Other, not assigned',
+}
+
+def normalize_caseless(s):
+    '''
+    Shifts the string into a uniform case (lowercase),
+    but also accounting for unicode characters. Used
+    for case-insenstive comparisons.
+    '''
+
+    return unicodedata.normalize('NFKD', s.casefold())
+
+def unicode_block(s):
+    ''' Gets the name of the Unicode block that contains the given character. '''
+
+    codepoint = ord(s)
+    index = bisect(UNICODE_BLOCK_STARTS, codepoint)
+    try:
+        start, stop, block = UNICODE_BLOCKS[index]
+    except IndexError:
+        return None
+
+    return block if codepoint <= stop else None
+
+def unicode_repr(s):
+    '''
+    Similar to repr(), but always escapes characters that aren't "readable".
+    That is, any characters not in READABLE_CHAR_SET.
+    '''
+
+    parts = []
+    for ch in s:
+        if ch == '\n':
+            parts.append('\\n')
+        elif ch == '\t':
+            parts.append('\\t')
+        elif ch == '"':
+            parts.append('\\"')
+        elif ch in READABLE_CHAR_SET:
+            parts.append(ch)
+        else:
+            num = ord(ch)
+            if num < 0x100:
+                parts.append(f'\\x{num:02x}')
+            elif num < 0x10000:
+                parts.append(f'\\u{num:04x}')
+            elif num < 0x100000000:
+                parts.append(f'\\U{num:08x}')
+            else:
+                raise ValueError(f"Character {ch!r} (ord {num:x}) too big for escaping")
+
+    escaped = ''.join(parts)
+    return f'"{escaped}"'
+
+    def user_disc(user):
+        '''
+        Return the user's username and disc
+        in the format username#disc
+        '''
+
+        return f'{user.name}#{user.discriminator}'
