b4f2f3ff4dc39a3f0fac36d6743f874286cc8520
Ammon Smith <ammon.i.smith@gmail.com>
2018-09-21 11:11:04 -0400

    Add StringBuilder.

--- futaba/__init__.py
+++ futaba/__init__.py
@@ -14,7 +14,7 @@
 futaba - A Discord Mod bot for the Programming server
 '''
 
-from . import client, config, download, enums, journal, parse, permissions, unicode, utils
+from . import client, config, download, enums, journal, parse, permissions, str_builder, unicode, utils
 
 __all__ = [
     '__version__',
@@ -25,8 +25,9 @@ __all__ = [
     'journal',
     'parse',
     'permissions',
+    'str_builder',
     'unicode',
     'utils',
 ]
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
