ca20e3e505d8b30ad721bb7f5470c5c7ec23146b
Ammon Smith <ammon.i.smith@gmail.com>
2018-12-01 02:48:40 -0500

    Fix emoji reference.

--- futaba/cogs/info/core.py
+++ futaba/cogs/info/core.py
@@ -83,7 +83,7 @@ class Info(AbstractCog):
             embed.description = str(emoji)
             embed.set_author(name=name)
             embed.set_thumbnail(url=emoji.url)
-            embed.add_field(name="Name", value=f"`emoji.name`")
+            embed.add_field(name="Name", value=f"`{emoji.name}`")
             embed.add_field(name="Guild", value=emoji.guild.name)
             embed.add_field(name="ID", value=str(emoji.id))
             embed.add_field(name="Managed", value=lowerbool(emoji.managed))
