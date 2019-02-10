7e6aaac19eaf7a5f3fa90769e5877827086c8f38
Ammon Smith <ammon.i.smith@gmail.com>
2019-02-10 01:24:44 -0500

    Update copyright years.

--- futaba/cogs/roles/core.py
+++ futaba/cogs/roles/core.py
@@ -2,7 +2,7 @@
 # cogs/roles/core.py
 #
 # futaba - A Discord Mod bot for the Programming server
-# Copyright (c) 2017-2018 Jake Richardson, Ammon Smith, jackylam5
+# Copyright (c) 2017-2019 Jake Richardson, Ammon Smith, jackylam5
 #
 # futaba is available free of charge under the terms of the MIT
 # License. You are free to redistribute and/or modify it under those
