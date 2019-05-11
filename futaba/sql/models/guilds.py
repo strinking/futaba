76feb13b3f58c949326baae2447b1721c5f4ec5c
Ammon Smith <ammon.i.smith@gmail.com>
2019-05-11 18:47:46 -0400

    Add 'active' field to guilds.

--- futaba/sql/models/guilds.py
+++ futaba/sql/models/guilds.py
@@ -23,7 +23,7 @@ import functools
 import logging
 from collections import namedtuple
 
-from sqlalchemy import BigInteger, Column, Table
+from sqlalchemy import Boolean, BigInteger, Column, Table
 from sqlalchemy.sql import select
 
 from ..hooks import run_hooks
@@ -41,7 +41,10 @@ class GuildsModel:
     def __init__(self, sql, meta):
         self.sql = sql
         self.tb_guilds = Table(
-            "guilds", meta, Column("guild_id", BigInteger, primary_key=True)
+            "guilds",
+            meta,
+            Column("guild_id", BigInteger, primary_key=True),
+            Column("active", Boolean, default=True),
         )
 
     # Note: Do NOT register the on_guild_join/on_guild_leave hooks here,
@@ -50,17 +53,35 @@ class GuildsModel:
 
     def add_guild(self, guild):
         logger.info("Adding guild '%s' (%d) to guilds list", guild.name, guild.id)
-        ins = self.tb_guilds.insert().values(guild_id=guild.id)
-        self.sql.execute(ins)
+
+        if guild.id in self.get_guild_ids():
+            upd = (
+                self.tb_guilds.update()
+                .values(active=True)
+                .where(self.tb_guilds.c.guild_id == guild.id)
+            )
+            self.sql.execute(upd)
+        else:
+            ins = self.tb_guilds.insert().values(guild_id=guild.id)
+            self.sql.execute(ins)
+
         run_hooks("on_guild_join", guild)
 
     def remove_guild(self, guild):
         logger.info("Removing guild '%s' (%d) from guilds list", guild.name, guild.id)
         run_hooks("on_guild_leave", guild)
+
+        upd = (
+            self.tb_guilds.update()
+            .values(active=False)
+            .where(self.tb_guilds.c.guild_id == guild.id)
+        )
+        self.sql.execute(upd)
+
         delet = self.tb_guilds.delete().where(self.tb_guilds.c.guild_id == guild.id)
         self.sql.execute(delet)
 
-    def get_guild_ids(self, bot):
+    def get_guild_ids(self):
         logger.info("Fetching guilds currently in database")
         sel = select([self.tb_guilds.c.guild_id])
         result = self.sql.execute(sel)
@@ -74,7 +95,7 @@ class GuildsModel:
         )
 
     def migrate(self, bot):
-        migrated_guild_ids = frozenset(self.get_guild_ids(self))
+        migrated_guild_ids = frozenset(self.get_guild_ids())
         current_guild_ids = frozenset(guild.id for guild in bot.guilds)
 
         logger.info(
