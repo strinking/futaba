18fcf145042933462fdf5ff300259b3d5d1ed876
Ammon Smith <ammon.i.smith@gmail.com>
2019-05-27 14:27:05 -0400

    Add first statbot query command.

--- futaba/cogs/optional/statbot/sql.py
+++ futaba/cogs/optional/statbot/sql.py
@@ -17,6 +17,9 @@ Module for abstractly interfacing with Statbot's RDBMS.
 import logging
 
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+
+from .utils import int_hash
 
 logger = logging.getLogger(__name__)
 
@@ -37,3 +40,42 @@ class StatbotSqlHandler:
 
     def execute(self, *args, **kwargs):
         return self.conn.execute(*args, **kwargs)
+
+    # Specific queries
+    def message_count(self, guild, user, excluded_channels=()):
+        """
+        Determines how many messages a user has in the guild.
+        A list of excluded channels can be specified.
+        """
+
+        logger.info(
+            "Querying message count for user '%s' (%d) in guild '%s' (%d)",
+            user.name,
+            user.id,
+            guild.name,
+            guild.id,
+        )
+
+        stmt = text(
+            """
+            SELECT
+                COUNT(message_id) AS messages,
+                COUNT(edited_at) AS edited,
+                COUNT(deleted_at) AS deleted
+            FROM messages
+            WHERE guild_id = :guild_id
+                AND int_user_id = :user_id
+                AND channel_id NOT IN :excluded_channel_ids
+        """
+        )
+
+        excluded_channel_ids = tuple(channel.id for channel in excluded_channels)
+        result = self.conn.execute(
+            stmt,
+            guild_id=guild.id,
+            user_id=int_hash(user.id),
+            excluded_channel_ids=excluded_channel_ids or (0,),
+        )
+
+        message_count, edited_count, deleted_count = result.fetchone()
+        return message_count, edited_count, deleted_count
