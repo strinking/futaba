23769b7924d1163c4cf457024a30a1326e0231ac
Josh Stockin <joshstockin@gmail.com>
2020-01-25 22:38:59 -0600

    Add compiled list of words to use in simplewriter

---
+++ futaba/cogs/optional/simplewriter/words/__init__.py
@@ -0,0 +1,17 @@
+#
+# cogs/optional/simplewriter/words/__init__.py
+#
+# futaba - A Discord Mod bot for the Programming server
+# Copyright (c) 2017-2020 Jake Richardson, Ammon Smith, jackylam5, Joshua 'joshuas3' Stockin
+#
+# futaba is available free of charge under the terms of the MIT
+# License. You are free to redistribute and/or modify it under those
+# terms. It is distributed in the hopes that it will be useful, but
+# WITHOUT ANY WARRANTY. See the LICENSE file for more details.
+#
+
+__all__ = [
+    "core_words_list",
+]
+
+from .core_words_list import core_words_list
