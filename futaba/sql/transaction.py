5d8857588b3159856d0ea0d34cb6564ac691621b
Ammon Smith <ammon.i.smith@gmail.com>
2020-05-08 04:41:11 -0400

    Update copyright year to 2020.

--- futaba/sql/transaction.py
+++ futaba/sql/transaction.py
@@ -2,7 +2,7 @@
 # sql/transaction.py
 #
 # futaba - A Discord Mod bot for the Programming server
-# Copyright (c) 2017-2019 Jake Richardson, Ammon Smith, jackylam5
+# Copyright (c) 2017-2020 Jake Richardson, Ammon Smith, jackylam5
 #
 # futaba is available free of charge under the terms of the MIT
 # License. You are free to redistribute and/or modify it under those
