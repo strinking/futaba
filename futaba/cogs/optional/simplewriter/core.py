33b7c359cd2e5229d7fe26a0eecb5ed2f1d15b52
Josh Stockin <joshstockin@gmail.com>
2020-08-01 09:30:29 -0500

    Merge listener methods into cog
    Instead of making them partials

--- futaba/cogs/optional/simplewriter/core.py
+++ futaba/cogs/optional/simplewriter/core.py
@@ -17,20 +17,17 @@ the first ten hundred most used words of the language.
 (https://xkcd.com/simplewriter)
 """
 
-from futaba.utils import async_partial
 from futaba.cogs.abc import AbstractCog
 
-from .listeners import on_message, on_message_edit
+from .simple_filter import simple_filter
 
 
 class SimplewriterCog(AbstractCog):
-    __slots__ = ("journal", "on_message", "on_message_edit", "channel_id")
+    __slots__ = ("journal", "channel_id")
 
     def __init__(self, bot):
         super().__init__(bot)
         self.journal = bot.get_broadcaster("/simplewriter")
-        self.on_message = async_partial(on_message, self)
-        self.on_message_edit = async_partial(on_message_edit, self)
         self.channel_id = int(bot.config.optional_cogs["simplewriter"]["channel-id"])
 
     def setup(self):
@@ -39,3 +36,10 @@ class SimplewriterCog(AbstractCog):
     def cog_unload(self):
         self.bot.remove_listener(self.on_message, "on_message")
         self.bot.remove_listener(self.on_message_edit, "on_message_edit")
+
+    async def on_message(self, message):
+        await simple_filter(self, message)
+
+    async def on_message_edit(self, before, after):
+        if after:
+            await simple_filter(self, after)
