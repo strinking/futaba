ecc01325b0747e46896bcb5fdf6408d9fc21dbc0
Ignas Maslinskas <5737899+Hoffs@users.noreply.github.com>
2021-10-26 19:16:14 +0300

    Add cog for handling cross-channel spam (#381)
    Add cog that keeps track of limited message history per user and
    tries to identify if user is cross-posting same message to different
    channels. If determinted that same message was posted to set amount
    (5) of channels, user is punished and messages are deleted.

---
+++ futaba/cogs/crosspost/__init__.py
@@ -0,0 +1,11 @@
+from .core import Crosspost
+
+# Setup for when cog is loaded
+def setup(bot):
+    cog = Crosspost(bot)
+    bot.add_cog(cog)
+
+# Remove all the cogs when cog is unloaded
+def teardown(bot):
+    bot.remove_cog(Crosspost.__name__)
+
