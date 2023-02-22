strinking/futaba

commit 7133f39127ff14251753fcaef47980d1afb73ac5

Add nonpurge role to settings sql (data and model)

futaba/sql/models/settings.py
@@ -100,6 +100,7 @@ class SettingsModel:
             Column("mute_role_id", BigInteger, nullable=True),
             Column("jail_role_id", BigInteger, nullable=True),
             Column("focus_role_id", BigInteger, nullable=True),
+            Column("nonpurge_role_id", BigInteger, nullable=True),
             # Ensures special roles aren't assigned to @everyone
             CheckConstraint(
                 "member_role_id is NULL OR member_role_id != guild_id",
@@ -121,6 +122,10 @@ class SettingsModel:
                 "focus_role_id is NULL or focus_role_id != guild_id",
                 name="special_role_focus_not_everyone_check",
             ),
+            CheckConstraint(
+                "nonpurge_role_id is NULL or nonpurge_role_id != guild_id",
+                name="special_role_nonpurge_not_everyone_check",
+            ),
             # Ensures Guest and punishment roles aren't the same as the Member role
             CheckConstraint(
                 "guest_role_id is NULL OR guest_role_id != member_role_id",
@@ -137,6 +142,10 @@ class SettingsModel:
             CheckConstraint(
                 "focus_role_id is NULL OR focus_role_id != member_role_id",
                 name="special_role_focus_not_member_check",
+            ),
+            CheckConstraint(
+                "nonpurge_role_id is NULL OR nonpurge_role_id != member_role_id",
+                name="special_role_nonpurge_not_member_check",
             ),
         )
         self.tb_reapply_roles = Table(
@@ -355,6 +364,7 @@ class SettingsModel:
             mute_role_id=None,
             jail_role_id=None,
             focus_role_id=None,
+            nonpurge_role_id=None,
         )
         self.sql.execute(ins)
         self.special_roles_cache[guild] = SpecialRoleData(guild, None, None, None, None)
@@ -370,6 +380,7 @@ class SettingsModel:
                 self.tb_special_roles.c.mute_role_id,
                 self.tb_special_roles.c.jail_role_id,
                 self.tb_special_roles.c.focus_role_id,
+                self.tb_special_roles.c.nonpurge_role_id,
             ]
         ).where(self.tb_special_roles.c.guild_id == guild.id)
         result = self.sql.execute(sel)
@@ -378,9 +389,9 @@ class SettingsModel:
             self.add_special_roles(guild)
             return self.special_roles_cache[guild]
 
-        member_role_id, guest_role_id, mute_role_id, jail_role_id, focus_role_id = result.fetchone()
+        member_role_id, guest_role_id, mute_role_id, jail_role_id, focus_role_id, nonpurge_role_id = result.fetchone()
         roles = SpecialRoleData(
-            guild, member_role_id, guest_role_id, mute_role_id, jail_role_id, focus_role_id
+            guild, member_role_id, guest_role_id, mute_role_id, jail_role_id, focus_role_id, nonpurge_role_id
         )
         self.special_roles_cache[guild] = roles
         return roles
@@ -391,7 +402,7 @@ class SettingsModel:
 
         values = {}
         for attr, role in attrs.items():
-            assert attr in ("member", "guest", "mute", "jail", "focus"), "Unknown column"
+            assert attr in ("member", "guest", "mute", "jail", "focus", "nonpurge"), "Unknown column"
             values[f"{attr}_role_id"] = getattr(role, "id", None)
 
         upd = (

futaba/sql/data/settings.py
@@ -54,10 +54,10 @@ class ReapplyRolesData:
 
 
 class SpecialRoleData:
-    __slots__ = ("guild", "member_role", "guest_role", "mute_role", "jail_role", "focus_role")
+    __slots__ = ("guild", "member_role", "guest_role", "mute_role", "jail_role", "focus_role", "nonpurge_role")
 
     def __init__(
-        self, guild, member_role_id, guest_role_id, mute_role_id, jail_role_id, focus_role_id
+        self, guild, member_role_id, guest_role_id, mute_role_id, jail_role_id, focus_role_id, nonpurge_role_id
     ):
         self.guild = guild
         self.member_role = self._get_role(member_role_id)
@@ -65,6 +65,7 @@ class SpecialRoleData:
         self.mute_role = self._get_role(mute_role_id)
         self.jail_role = self._get_role(jail_role_id)
         self.focus_role = self._get_role(focus_role_id)
+        self.nonpurge_role = self._get_role(nonpurge_role_id)
 
     def update(self, attrs):
         logger.debug("Updating special role storage: %s", attrs)
@@ -78,6 +79,8 @@ class SpecialRoleData:
             self.jail_role = attrs["jail"]
         if "focus" in attrs:
             self.focus_role = attrs["focus"]
+        if "nonpurge" in attrs:
+            self.nonpurge_role = attrs["nonpurge"]
 
     @property
     def member(self):
@@ -94,10 +97,14 @@ class SpecialRoleData:
     @property
     def jail(self):
         return self.jail_role
-    
+
     @property
     def focus(self):
         return self.focus_role
+
+    @property
+    def nonpurge(self):
+        return self.nonpurge_role
 
     def _get_role(self, id):
         if id is None:
@@ -111,6 +118,7 @@ class SpecialRoleData:
         yield self.mute_role
         yield self.jail_role
         yield self.focus_role
+        yield self.nonpurge_role
 
 
 class TrackingBlacklistData:

commit 8109b3b4acad51b8a3e8e87b3e31b27b85bb4400

fix init of SpecialRoleData in add_special_roles
this appears to have been broken as far back as 80a04d13 and then
further broken in 7133f391

futaba/sql/models/settings.py
@@ -367,7 +367,9 @@ class SettingsModel:
             nonpurge_role_id=None,
         )
         self.sql.execute(ins)
-        self.special_roles_cache[guild] = SpecialRoleData(guild, None, None, None, None)
+        self.special_roles_cache[guild] = SpecialRoleData(
+            guild, None, None, None, None, None, None
+        )
 
     def get_special_roles(self, guild):
         if guild in self.special_roles_cache: