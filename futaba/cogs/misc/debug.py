316bbaa0a9d7e9ddbbd20bd7b26ceca70a949d82
emmiegit <emmie.maeda@gmail.com>
2024-01-11 03:18:18 -0500

    Update my name in copyright notices (#394)
    * Update aismallard's name in copyright headers.
    
    * Add .mailmap file

--- futaba/cogs/misc/debug.py
+++ futaba/cogs/misc/debug.py
@@ -2,7 +2,7 @@
 # cogs/misc/debug.py
 #
 # futaba - A Discord Mod bot for the Programming server
-# Copyright (c) 2017-2020 Jake Richardson, Ammon Smith, jackylam5
+# Copyright (c) 2017-2020 Jake Richardson, Emmie Smith, jackylam5
 #
 # futaba is available free of charge under the terms of the MIT
 # License. You are free to redistribute and/or modify it under those
